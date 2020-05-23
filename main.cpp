734da2548a5660332e531d60c766731f39ecc26a
valentinGenov <valbg0110@gmail.com>
2020-05-23 11:20:03 +0300

    .cpp file of the algorithms

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int presidents = 0;
+int procentOpasnost = 0;
+int addDel = rand() % 5 + 1;
+
+//proverka za dobavqne na prezident/mahane
+int givepresidents() {
+    if (procentOpasnost1 > 50 || procentOpasnost2) {
+        presidents = presidents - addDel;
+    } else if (procentOpasnost1 < 50 || procentOpasnost2) {
+        presidents = presidents + addDel;
+    }
+}
+
+//proverka za pobeda/zaguba
+int checkwin() {
+    if (presidents >= 0) {
+        cout<<"You lose... All presidents are dead!";
+    } else {
+        cout<<"You finished this round with "<<presidents<<"!";
+    }
+}
+
+int durjavi() {
+    int durjava1;
+    int durjava2;
+    int procentOpasnost1;
+    int procentOpasnost2;
+
+
+}
+
+
+int main()
+{
+
+cout<<"Let's save all the presidents!"
+
+    for (int i = 1; i <= 18) {
+
+
+
+    }
+
+}
